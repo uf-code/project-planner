5216ddfb4fd5a956a80792aa89bd447cbb30757f
uf-code <ugur.firat.education@gmail.com>
2021-03-08 08:32:22 +0300

    Creating a Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar },
+}
+</script>
+
 <style>
 body {
-  background: f2f2f2;
+  background: #f2f2f2;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
