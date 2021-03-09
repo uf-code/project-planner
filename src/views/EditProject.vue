c0dadf1bbf383f7803ab6bbfc0b0f1ced94a1cbe
uf-code <ugur.firat.education@gmail.com>
2021-03-09 08:33:16 +0300

    giving a functionality to editproject component

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -19,6 +19,17 @@ export default {
       uri: 'http://localhost:3000/projects/' + this.id,
     }
   },
+  methods: {
+    handleSubmit() {
+      fetch(this.uri, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: this.title, details: this.details }),
+      })
+        .then(() => this.$router.push('/'))
+        .catch((err) => console.error(err.message))
+    },
+  },
   mounted() {
     fetch(this.uri)
       .then((res) => res.json())
